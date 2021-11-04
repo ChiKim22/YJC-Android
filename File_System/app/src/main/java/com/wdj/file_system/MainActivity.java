cd50cf7df0557c2f7cea9938f9511bed89d01652
ChiKim22 <wlsgh2505@gmail.com>
2021-11-04 12:15:18 +0900

    File_System (1104 + EditText to change file content)

--- File_System/app/src/main/java/com/wdj/file_system/MainActivity.java
+++ File_System/app/src/main/java/com/wdj/file_system/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
@@ -15,6 +16,7 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     Button btnWirte, btnRead;
+    EditText ed1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnWirte = (Button) findViewById(R.id.btnWrite);
         btnRead = (Button) findViewById(R.id.btnRead);
+        ed1 = (EditText) findViewById(R.id.ed1);
 
         btnWirte.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
                     FileOutputStream outFs = openFileOutput("file.txt", Context.MODE_PRIVATE);
                     // 파일을  때에는 Byte 형으로, 쓸 때에는 Byte 에서 다른 형으로 형 변환 필요.
 
-                    String str = "Android!";
+                    String str = ed1.getText().toString();
                     outFs.write(str.getBytes());
-                    outFs.close();
+                    outFs.close(); // 파일을 닫아줌.
                     Toast.makeText(getApplicationContext(),
                             "file.txt Create Complete.",
                                         Toast.LENGTH_SHORT).show();
@@ -50,11 +53,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     FileInputStream inFs = openFileInput("file.txt");
-                    byte[] txt = new byte[38];
+                    byte[] txt = new byte[38]; // 읽을 때 byte 배열 형으로 읽어서 이를 String 으로 변환해서 출력 해준다.
                     inFs.read(txt);
                     String str = new String(txt);
 
-                    Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "content : " + str, Toast.LENGTH_SHORT).show();
                 } catch (IOException e){
                     Toast.makeText(getApplicationContext(),
                             "No File...", Toast.LENGTH_SHORT).show();
