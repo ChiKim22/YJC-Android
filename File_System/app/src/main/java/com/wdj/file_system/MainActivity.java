2f91a75014f89f5e215334796bfa45c097652e69
ChiKim22 <wlsgh2505@gmail.com>
2021-11-04 12:18:43 +0900

    File_System (1104 + EditText to change file content)

--- File_System/app/src/main/java/com/wdj/file_system/MainActivity.java
+++ File_System/app/src/main/java/com/wdj/file_system/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                                         Toast.LENGTH_SHORT).show();
 
                 }catch (IOException e){
-
+                    e.printStackTrace();
                 }
             }
         });
