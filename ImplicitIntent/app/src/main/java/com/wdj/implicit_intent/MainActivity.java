0cf0b847fe735086853f8855787d8cb98c03d7b6
ChiKim22 <wlsgh2505@gmail.com>
2021-11-16 12:35:55 +0900

    Implicit Intent (2021-11-16)

---
+++ ImplicitIntent/app/src/main/java/com/wdj/implicit_intent/MainActivity.java
@@ -0,0 +1,88 @@
+package com.wdj.implicit_intent;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Implicit Intent");
+
+        Button btnDial = (Button) findViewById(R.id.btnDial);
+        Button btnWeb = (Button) findViewById(R.id.btnWeb);
+        Button btnGoogle = (Button) findViewById(R.id.btnGoogle);
+        Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        Button btnSms = (Button) findViewById(R.id.btnSms);
+        Button btnPhoto = (Button) findViewById(R.id.btnPhoto);
+
+
+        //Action Button
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:01012345678");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.naver.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://maps.google.co.kr/maps?q="
+                        + 35.89578 + "," + 128.62244 + "&z=15"); // Gmap Gps Location
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEARCH);
+                intent.putExtra(SearchManager.QUERY, "Android");
+                startActivity(intent);
+            }
+        });
+
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.putExtra("sms_body", "Hello World!!");
+                intent.setData(Uri.parse("smsto:" + Uri.encode("010-1234-5678")));
+                startActivity(intent);
+            }
+        });
+
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
