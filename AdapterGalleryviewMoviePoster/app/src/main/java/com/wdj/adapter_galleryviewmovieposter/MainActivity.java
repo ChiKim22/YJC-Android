5d866442f77d378bfc5231bab78d15fec1b99144
ChiKim22 <wlsgh2505@gmail.com>
2021-11-23 12:15:50 +0900

    Movie Poster (Gallery View)

---
+++ AdapterGalleryviewMoviePoster/app/src/main/java/com/wdj/adapter_galleryviewmovieposter/MainActivity.java
@@ -0,0 +1,76 @@
+package com.wdj.adapter_galleryviewmovieposter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Movie Poster Gallery");
+
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+
+        MyGalleryAdapter myGalleryAdapter = new MyGalleryAdapter(this);
+        gallery.setAdapter(myGalleryAdapter);
+
+
+    }
+
+    public class MyGalleryAdapter extends BaseAdapter {
+
+        Context context;
+
+        Integer[] posterID = { R.drawable.mov01, R.drawable.mov02,
+                R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+                R.drawable.mov06, R.drawable.mov07, R.drawable.mov08,
+                R.drawable.mov09, R.drawable.mov10 };
+
+        public MyGalleryAdapter(Context c) {
+            context = c;
+        }
+
+        public int getCount() {
+            return posterID.length;
+        }
+
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageview = new ImageView(context);
+            imageview.setLayoutParams(new Gallery.LayoutParams(200, 300));
+            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageview.setPadding(5, 5, 5, 5);
+            imageview.setImageResource(posterID[position]);
+
+            final int pos = position; // 갤러리뷰에서는 좌, 우로 스와이프 해주기 때문에 TouchListener 를 사용.
+            imageview.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    ImageView ivPoster = (ImageView) findViewById(R.id.ivPoster);
+                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    ivPoster.setImageResource(posterID[pos]);
+                    return false;
+                }
+            });
+
+            return imageview;
+        }
+    }
+}
