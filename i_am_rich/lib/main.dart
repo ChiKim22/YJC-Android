bc3d157d22265bb5f0873763ba844848f8b1320c
ChiKim22 <wlsgh2505@gmail.com>
2022-01-06 16:31:35 +0900

    I Am Rich(flutter)

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //Starting point {}를 => 로 바꿀 수 있음.
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
